a28559eb7a742732cdb3f703a2800b6e8bd3eefa
Alfonso Reyes <arc980103@gmail.com>
2019-04-06 12:58:16 -0700

    Small changes to pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,12 @@
 module.exports = {
   apps: [{
-    name: 'api',
+    name: 'mango',
     script: './dist/server.js',
     instances: 4,
     exec_mode: 'cluster',
+    autorestart: true,
     watch: false,
+    max_memory_restart: '1G',
     env: {
       NODE_ENV: 'development'
     },
